612941d9db530ed06ee1e1db15c6ae15e7cf5b07
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-04-23 21:22:13 +0000

    Tue, Apr 23, 2024, 2:22 PM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,77 @@
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['aplication', 'programing', 'interface', 'wizard']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+//show hidden word
+function displayWord(){
+    wordEl.innerHTML = `
+    ${selectedWord
+        .split('')
+        .map(letter => `
+    <span class = "letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+    </span>
+
+    `).join('')}
+
+    `
+    const innerWord=wordEl.innertext.replace(/\n/g, '')
+    console.log(wordEl.innerText, innerWord)
+    if(innerWord == selectedWord){
+        finalMessage.innerText = 'congratulations You won'
+        popup.style.display = 'flex'
+    }
+}
+//update the wrong numebrs
+function updateWrongLettersEl(){
+    console.log('tbdwrong')
+}
+
+//show notif
+function showNotification(){
+    notification.classList.add('show')
+
+    setTimeout(() =>{
+        notification.classList.remove('show')
+    }, 2000)
+}
+
+//keydown letter press
+window.addEventListener('keydown', e=>{
+    console.log(e.keyCode)
+    if (e.keyCode >= 65 && e.keyCode <=90){
+        const letter = e.key
+
+        if(selectedWord.includes(letter)){
+            if( !correctLetters.includes(letter)){
+                correctLetters.push(letter)
+
+                displayWord()
+            }else{
+                showNotification()
+            }
+        } else{
+            if (!wrongLetters.include(letter)){
+                wrongLetters.push(letter)
+
+                updateWrongLettersEl()
+            }else{
+                showNotification()
+            }
+        }
+    }
+})
+
+displayWord()
